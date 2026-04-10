faf1cecef168d477ca862c045ad177bf9640f5a9
Hugo2203 <6286734@cegepmontpetit.ca>
2026-04-10 17:15:13 -0400

    changer message 3

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de l'environnement de DEV patate2 ",
+  dev: "Hello de l'environnement de DEV patate3",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
